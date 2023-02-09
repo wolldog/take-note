9339b3a1b0030cdbc0fdd9e3017f7e551c1713f0
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-02-09 15:50:44 +1100

    Add server.js; dependancies and server PORT

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+
+
+//Listen  
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT} 🚀`))
