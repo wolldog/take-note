767f688c2e94e6608b49d9e77ceb2b8e4c50bd57
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-02-09 16:31:42 +1100

    Mod. index.js; renderActiveNote to use note_id

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -52,8 +52,7 @@ const deleteNote = (id) =>
 
 const renderActiveNote = () => {
   hide(saveNoteBtn);
-
-  if (activeNote.id) {
+  if (activeNote.note_id) {
     noteTitle.setAttribute('readonly', true);
     noteText.setAttribute('readonly', true);
     noteTitle.value = activeNote.title;
@@ -83,7 +82,7 @@ const handleNoteDelete = (e) => {
   e.stopPropagation();
 
   const note = e.target;
-  const noteId = JSON.parse(note.parentElement.getAttribute('data-note')).id;
+  const noteId = JSON.parse(note.parentElement.getAttribute('data-note')).note_id;
 
   if (activeNote.id === noteId) {
     activeNote = {};
