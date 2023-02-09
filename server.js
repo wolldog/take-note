d37dc439f124f72bd047347f6e5f9492ff6dc62a
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-02-09 16:03:19 +1100

    Add POST route for new notes, append to db.json

--- server.js
+++ server.js
@@ -19,10 +19,28 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
+//Routes for api/notes
 // GET route to retreive all notes from db.json
 app.get('/api/notes', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
+
+// POST Route for a new note with unique id
+app.post('/api/notes', (req, res) => {
+  const {title, text} = req.body;
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4(),
+    };
+    // readAndAppend (see fsUtils) reads and appends db.json with new notes
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
 
 //Listen  
 const PORT = process.env.PORT || 5000
